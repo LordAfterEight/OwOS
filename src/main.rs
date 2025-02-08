18c5c096128040d67bccf3f787e065c7dc66361b
LordAfterEight <elias@stettmayer.de>
2025-02-08 21:16:25 +0100

    nothing happened really

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,20 @@ use core::panic::PanicInfo;
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {loop {}}
 
+fn print() {
+    static TEXT: &[u8] = b"{text}";
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in TEXT.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+}
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    print();
     loop {}
 }
