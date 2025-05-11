d03ae0f17ad16361977b38a6b48ece7222279ea6
LordAfterEight <elias@stettmayer.de>
2025-05-11 18:47:48 +0200

    Added a lot of stuff

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,17 @@
 #![no_std]
 #![no_main]
 use core::panic::PanicInfo;
+mod vga_buffer;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {loop {}}
-
-fn print() {
-    static TEXT: &[u8] = b"Hello World";
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in TEXT.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop{}
 }
+
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    print();
+    println!("OwOS => 'Hello World!'");
     loop {}
 }
