579c56479e5849aa1b195d4248f425ef3c1fb6a5
LordAfterEight <elias@stettmayer.de>
2025-05-12 10:24:39 +0200

    Some changes

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
 use core::panic::PanicInfo;
 mod vga_buffer;
+
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("OwOS => Hello World!");
+
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
+
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop{}
-}
-
-
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    println!("OwOS => 'Hello World!'");
-    loop {}
 }
